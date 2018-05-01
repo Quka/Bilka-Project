d428a805d3204bdec5471bc423ba9c5eb787f236
Arlind Ukshini <quka93@gmail.com>
2018-05-01 11:03:15 +0200

    PersistencyService init

--- Stock Management/Stock Management/PersistencyService/Interface/IPersistencyService.cs
+++ Stock Management/Stock Management/PersistencyService/Interface/IPersistencyService.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Stock_Management.Model;
 
 namespace Stock_Management.PersistencyService.Interface
 {
 	interface IPersistencyService
 	{
+		void InsertProductAsync(Product p);
+		void DeleteProductAsync(Product p);
+		void UpdateProductAsync(Product p);
+		ObservableCollection<Product> LoadProductsAsync();
+
+		void InsertOrder(Order o);
+		void UpdateOrder(Order o);
+		ObservableCollection<Order> LoadOrdersAsync();
+
+		ObservableCollection<ProductReturn> LoadProductReturnsAsync();
+
+		Employee GetUser(String username, String password);
 	}
 }
