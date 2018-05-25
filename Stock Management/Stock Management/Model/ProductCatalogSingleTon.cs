ef5472cd66f311a64a3867bf67e078163f63fe76
zakikakar <36473212+zakikakar@users.noreply.github.com>
2018-05-25 10:50:02 +0200

    k

--- Stock Management/Stock Management/Model/ProductCatalogSingleTon.cs
+++ Stock Management/Stock Management/Model/ProductCatalogSingleTon.cs
@@ -124,9 +124,6 @@ namespace Stock_Management.Model
 			    List<Product> products = await PersistencyService.LoadProductsAsync();
 			    foreach (Product p in products)
 			    {
-                    // TODO delete line below
-                    p.GetOrderList();
-
 				    ProductList.Add(p);
 			    }
 		    }
